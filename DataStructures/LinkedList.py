29e3adceb4158e15e715da4594289b7ad75e4268
tgmoore <tgarrettmoore@gmail.com>
2019-02-15 17:34:58 -0600

    OOP pass
    More OOP oriented

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -14,11 +14,32 @@ class Node:
         
     def getData(self):
         return self.data
+    
+    def getLeft(self):
+        return self.left
+    
+    def getRight(self):
+        return self.right
+    
+    def setLeft(self, l):
+        self.left = l
+        
+    def setRight(self, r):
+        self.right = r
         
 class LinkedList:
     
     def __init__(self, data = "head"):
         self.head = Node(data)
+        
+    def search(self, data):
+        curr = self.head
+        while curr and curr.getData() is not data:
+            curr = curr.getRight();
+        else:
+            if curr:
+                return curr
+        return None            
         
     def getNode(self, index):
         retVal = self.head
@@ -32,18 +53,32 @@ class LinkedList:
             retVal = retVal.right            
         return retVal.getData()
     
-    def addNode(self, n):
+    def addNode(self, data):
+        n = Node(data)
         tail = self.head
         while tail.right:
-            tail = tail.right
+            tail = tail.getRight
         else:
-            n.left = tail
-            tail.right = n
+            n.setLeft(tail)
+            tail.setRight(n)
+            
+    def deleteData(self, data):
+        curr = self.head
+        if curr.getData() is data:
+            self.head = self.head.getRight()
+        else:
+            curr = curr.getRight()
+            while curr:
+                if curr.getData() is data:
+                    if curr.getRight() is not None:
+                        curr.getRight().setLeft(curr.getLeft())
+                    curr.getLeft.setRight(curr.getRight())
             
     def deleteNode(self, index):
         retVal = self.head
         for i in range(0, index):
-            retVal = retVal.right
-        if retVal.right is not None:
-            retVal.right.left = retVal.left
-        retVal.left.right = retVal.right
+            retVal = retVal.getRight()
+        if retVal.getRight() is not None:
+            retVal.getRight.setLeft(retVal.getLeft())
+        retVal.getLeft().setRight(retVal.getRight())
+        
